702b3b9283ae8c3b7c4a0053af1b891812c2f15a
Nehasrs <nehasrs2005@gmail.com>
2026-07-22 10:41:14 +0530

    Week 5 - Angular Hands-On 4 - Template Driven Forms and Validation

--- Week_5_&_6/Angular_HandsOn/student-course-portal/src/app/app.routes.ts
+++ Week_5_&_6/Angular_HandsOn/student-course-portal/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { CourseListComponent } from './pages/course-list/course-list';
+import { StudentProfileComponent } from './pages/student-profile/student-profile';
+import { EnrollmentFormComponent } from './pages/enrollment-form/enrollment-form';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'courses',
+    component: CourseListComponent
+  },
+  {
+    path: 'profile',
+    component: StudentProfileComponent
+  },
+  {
+    path: 'enroll',
+    component: EnrollmentFormComponent
+  }
+];
--- Week_5_&_6/Angular_HandsOn/student-course-portal/src/app/app.ts
+++ Week_5_&_6/Angular_HandsOn/student-course-portal/src/app/app.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CourseListComponent} from './pages/course-list/course-list';
-import { HeaderComponent } from './components/header/header';
-import { HomeComponent } from './pages/home/home';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    HeaderComponent,
-    HomeComponent,
-    CourseListComponent
+    Header,
+    RouterOutlet
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+}
